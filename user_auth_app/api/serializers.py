ea705b30a7ae65a63871d0b2d3c66727247f6b7d
Christoph <c.wagner.germany@gmail.com>
2024-12-08 16:07:34 +0100

    token6

--- user_auth_app/api/serializers.py
+++ user_auth_app/api/serializers.py
@@ -6,3 +6,34 @@ class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
         fields = ['user', 'bio', 'location']
+        
+class RegistrationSerializer(serializers.ModelSerializer):
+    
+    repeated_password = serializers.CharField(write_only=True)
+    
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'repeated_password']
+        extra_kwargs = {
+            'password': {
+                'write_only': True
+            }
+        }
+        
+        def save(self):
+            pw = self.validated_data['password']
+            repeated_pw = self.validated_data['repeated_password']
+            
+            if pw != repeated_pw:
+                raise serializers.ValidationError({'error':'passwords dont match'})
+            
+            account = User(email=self.validated_data['email'], username=self.validated_data['username'])
+            account.set_password(pw)
+            account.save()
+            return account
+            
+            # email = self.validated_data['email']
+            
+            # if not email:
+            #     raise serializers.ValidationError({'error':'no email'})
+                
