d360e64c631f58d00630b9fd83194c0fd3acdf15
Christoph <c.wagner.germany@gmail.com>
2024-12-09 16:56:17 +0100

    login2

--- user_auth_app/api/views.py
+++ user_auth_app/api/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.response import Response
 from user_auth_app.models import UserProfile
 from .serializers import UserProfileSerializer, RegistrationSerializer
@@ -13,6 +14,26 @@ class UserProfileList(generics.ListCreateAPIView):
 class UserProfileDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
+    
+class CustomLoginView(ObtainAuthToken):
+    permission_classes = [AllowAny]
+    
+    def post(self, request):
+        serilalizer = self.serializer_class(data=request.data)
+        
+        data = {}
+        if serilalizer.is_valid():
+            user = serilalizer.validated_data['user']
+            token, created = Token.objects.get_or_create(user=user)
+            data = {
+                'token': token.key,
+                'username': user.username,
+                'email': user.email,
+            }
+        else:
+            data=serilalizer.errors
+            
+        return Response(data)
     
 class RegistrationView(APIView):
     permission_classes = [AllowAny]
